03818f67a6138cde583d8428be1885b9b6661f71
xuanhau1995 <77385287+xuanhau1995@users.noreply.github.com>
2021-03-22 14:47:37 +0700

    login page ui

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,222 +1,131 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-// import {
-//   CButton,
-//   CCard,
-//   CCardBody,
-//   CCardGroup,
-//   CCol,
-//   CContainer,
-//   CForm,
-//   CInput,
-//   CInputGroup,
-//   CInputGroupPrepend,
-//   CInputGroupText,
-//   CRow
-// } from '@coreui/react'
-// import CIcon from '@coreui/icons-react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import * as Icon from 'react-bootstrap-icons'
+import "./login.scss"
+import {
+  CButton,
+  CInputCheckbox,
+  CCard,
+  CFormGroup,
+  CCardBody,
+  CDropdownItem,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CLabel,
+  CForm,
+  CInput,
+  CAlert,
+  CInputGroup,
+  CInputGroupPrepend,
+  CPopover,
+  CInputGroupText,
+  CHeaderBrand,
+  CRow
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { useTranslation } from 'react-i18next';
+const Login = () => {
+  const { t, i18n } = useTranslation();
+  const placements = [
+    'bottom',
+  ]
+  return (
+    <div className="c-app c-default-layout flex-row align-items-start pt-4" >
+      <CContainer>
+        <CRow classname="d-flex justify-content-center">
+          <CCol col="6">
+            <CHeaderBrand className="mb-3 d-flex justify-content-center" to="">
+              <CIcon name="logoBg" height="120" alt="Logo-bg" />
+            </CHeaderBrand>
+          </CCol>
+        </CRow>
+        <CRow className="justify-content-center">
+          <CCol col="6" md="4">
+            <CCardGroup>
+              <CCard className="pt-0 pb-4 form-login">
+                <CCardBody className="pb-5">
+                  <CForm>
+                    <CRow>
+                      <CCol col="6" className="pb-4 pr-0">
+                        {placements.map(placement => {
+                          return (
+                            <CCol col="6" className="pb-4" key={placement}>
+                              <CPopover className="d-inline-block float-right"
 
-// const Login = () => {
-//   return (
-//     <div className="c-app c-default-layout flex-row align-items-center">
-//       <CContainer>
-//         <CRow className="justify-content-center">
-//           <CCol md="8">
-//             <CCardGroup>
-//               <CCard className="p-4">
-//                 <CCardBody>
-//                   <CForm>
-//                     <h1>Login</h1>
-//                     <p className="text-muted">Sign In to your account</p>
-//                     <CInputGroup className="mb-3">
-//                       <CInputGroupPrepend>
-//                         <CInputGroupText>
-//                           <CIcon name="cil-user" />
-//                         </CInputGroupText>
-//                       </CInputGroupPrepend>
-//                       <CInput type="text" placeholder="Username" autoComplete="username" />
-//                     </CInputGroup>
-//                     <CInputGroup className="mb-4">
-//                       <CInputGroupPrepend>
-//                         <CInputGroupText>
-//                           <CIcon name="cil-lock-locked" />
-//                         </CInputGroupText>
-//                       </CInputGroupPrepend>
-//                       <CInput type="password" placeholder="Password" autoComplete="current-password" />
-//                     </CInputGroup>
-//                     <CRow>
-//                       <CCol xs="6">
-//                         <CButton color="primary" className="px-4">Login</CButton>
-//                       </CCol>
-//                       <CCol xs="6" className="text-right">
-//                         <CButton color="link" className="px-0">Forgot password?</CButton>
-//                       </CCol>
-//                     </CRow>
-//                   </CForm>
-//                 </CCardBody>
-//               </CCard>
-//               <CCard className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
-//                 <CCardBody className="text-center">
-//                   <div>
-//                     <h2>Sign up</h2>
-//                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-//                       labore et dolore magna aliqua.</p>
-//                     <Link to="/register">
-//                       <CButton color="primary" className="mt-3" active tabIndex={-1}>Register Now!</CButton>
-//                     </Link>
-//                   </div>
-//                 </CCardBody>
-//               </CCard>
-//             </CCardGroup>
-//           </CCol>
-//         </CRow>
-//       </CContainer>
-//     </div>
-//   )
-// }
-
-// export default Login
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { login } from "../../../actions/auth";
-import classnames from "classnames";
-import SVGViewer from "src/helpers/svg.viewer";
-
-class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      phone: "",
-      password: "",
-      errors: {}
-    };
-  }
-
-  componentDidMount() {
-    // If logged in and user navigates to Login page, should redirect them to dashboard
-    if (this.props.auth.isAuthenticated) {
-      this.props.history.push("/dashboard");
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.auth.isAuthenticated) {
-      this.props.history.push("/dashboard");
-    }
-
-    if (nextProps.errors) {
-      this.setState({
-        errors: nextProps.errors
-      });
-    }
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.id]: e.target.value });
-  };
-
-  onSubmit = e => {
-    e.preventDefault();
-
-    const userData = {
-      phone: this.state.phone,
-      password: this.state.password
-    };
-
-    this.props.login(userData);
-  };
-
-  render() {
-    const { errors } = this.state;
-
-    return (
-      <div className="container">
-        <div style={{ marginTop: "4rem" }} className="col">
-          <div className="col s8 offset-s2">
-            <Link to="/" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Login</b> below
-              </h4>
-              <p className="grey-text text-darken-1">
-                Don't have an account? <Link to="/register">Register</Link>
-              </p>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.phone}
-                  error={errors.phone}
-                  id="phone"
-                  type="phone"
-                  className={classnames("", {
-                    invalid: errors.phone || errors.phonenotfound
-                  })}
-                />
-                <label htmlFor="phone">phone</label>
-                <span className="red-text">
-                  {errors.phone}
-                  {errors.phonenotfound}
-                </span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password || errors.passwordincorrect
-                  })}
-                />
-                <label htmlFor="password">Password</label>
-                <span className="red-text">
-                  {errors.password}
-                  {errors.passwordincorrect}
-                </span>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Login
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-        <div><SVGViewer name="msgPreview" width="300" text="Day la noi dung tinh dsfssf sdfsddf nhan sdlkfslkjfslk sljf sljf tin nhan."></SVGViewer></div>
-      </div>
-    );
-  }
+                                content={
+                                  <>
+                                    <CDropdownItem className="select-item">English</CDropdownItem>
+                                    <CDropdownItem className="select-item">Vietnamese</CDropdownItem>
+                                  </>
+                                }
+                                placement={placement}
+                                interactive={true}
+                                trigger="click"
+                              >
+                                <a className="d-inline-block float-right language pr-0">English<Icon.ChevronDown className="ml-2" /></a>
+                              </CPopover>
+                            </CCol>)
+                        })}
+                      </CCol>
+                    </CRow>
+                    <CRow>
+                      <CCol col="6">
+                        <CAlert color="danger">
+                          {t('login.msg-error')}
+                        </CAlert>
+                      </CCol>
+                    </CRow>
+                    <CRow className="d-flex justify-content-center">
+                      <CCol col="6" lg="0" className=" mt-2 pb-2 ">
+                        <h1 className="title-login mb-3 d-flex justify-content-center ">{t('login.title')}</h1>
+                        <h6 className="description mb-4  d-flex justify-content-center">{t('login.description')}</h6>
+                      </CCol>
+                    </CRow>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-user" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput type="text" placeholder={t('login.ph-username')} autoComplete="username" />
+                    </CInputGroup>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupPrepend>
+                        <CInputGroupText>
+                          <CIcon name="cil-lock-locked" />
+                        </CInputGroupText>
+                      </CInputGroupPrepend>
+                      <CInput type="password" placeholder={t('login.ph-password')} autoComplete="current-password" />
+                    </CInputGroup>
+                    {/* check login */}
+                    <CRow>
+                      <CCol col="3" lg="0" className="pt-0 pr-0 pl-3">
+                        <CFormGroup variant="custom-checkbox" inline>
+                          <CInputCheckbox
+                            custom
+                            id="inline-checkbox1"
+                            name="inline-checkbox1"
+                            value="option1"
+                          />
+                       <CLabel className="check-label-custom" variant="custom-checkbox" htmlFor="inline-checkbox1"><p>{t('login.lb-check-login')}</p></CLabel>
+                        </CFormGroup>
+                      </CCol>
+                      <CCol col="3" className="pt-0 pb-4"> <a href="##" className="text-ps d-inline-block float-right">{t('login.bt-forget-password')}</a></CCol>
+                    </CRow>
+                    {/* button login  */}
+                    <CCol col="6" lg="0" className="pt-2 pb-2">
+                      <CButton block color="primary">Log in</CButton>
+                    </CCol>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
 }
 
-Login.propTypes = {
-  login: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
-};
-
-const mapStateToProps = state => ({
-  auth: state.auth,
-  errors: state.errors
-});
-
-export default connect(
-  mapStateToProps,
-  { login }
-)(Login);
+export default Login
