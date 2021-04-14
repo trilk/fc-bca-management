ee5e7a7e497739c067646c2b0936eb3756dde36d
xuanhau1995 <77385287+xuanhau1995@users.noreply.github.com>
2021-04-14 13:07:49 +0700

    Update TheHeaderDropdown.js

--- src/containers/TheHeaderDropdown.js
+++ src/containers/TheHeaderDropdown.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as Icon from 'react-bootstrap-icons'
+// import './ModalSlider.scss'
+// import { ModalProfile } from '../views/modalCustom/ModalProfile'
 import {
   CBadge,
   CDropdown,
@@ -8,13 +10,22 @@ import {
   CDropdownToggle,
   CLink,
   CLabel,
-  CImg
+  CImg,
+  CButton,
+  CModalHeader,
+  CModalTitle,
+  CModalBody,
+  CModalFooter,
+  CModal,
+  CInput
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { useDispatch } from 'react-redux'
 import { useTranslation } from 'react-i18next';
 import { logout } from 'src/actions/auth'
 import { useHistory } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
 const TheHeaderDropdown = () => {
   const { t, i18n } = useTranslation();
@@ -29,40 +40,49 @@ const TheHeaderDropdown = () => {
   const pageNavigate = (page) => {
     history.push('/' + page);
   }
+  const [show, setShow] = useState(false)
+  const closeModalHandler = () => setShow(false)
 
   return (
-    <CDropdown
-      inNav
-      className="c-header-nav-items mx-2"
-      direction="down"
-    >
-      <CDropdownToggle className="c-header-nav-link drop-down" caret={false}>
-        <div className="pr-2">
-          <span className="text-muted">Hi: <strong>Admin</strong></span>
-        </div>
-        <div className="c-avatar">
-          <CImg
-            src={'avatars/6.jpg'}
-            className="c-avatar-img"
-            alt="admin@bootstrapmaster.com"
-          />
-        </div>
-      </CDropdownToggle>
-      <CDropdownMenu className="pt-2 mt-2" placement="bottom-end">
-        <CDropdownItem>
-          <CLink to="/users/:id"><CIcon name="cil-user" className="mfe-2" />{t('user-topmenu.item-profile-info')}</CLink>
-        </CDropdownItem>
-        <CDropdownItem onClick={() => pageNavigate("user-settings")}>
-          <CIcon name="cil-settings" className="mfe-2" />
-          {t('user-topmenu.item-setting')}
-        </CDropdownItem>
-        <CDropdownItem divider />
-        <CDropdownItem onClick={userLogout}>
-          <Icon.BoxArrowRight className="mfe-2" />
-          {t('user-topmenu.item-logout')}
-        </CDropdownItem>
-      </CDropdownMenu>
-    </CDropdown>
+    <>
+      <CDropdown
+        inNav
+        className="c-header-nav-items mx-2"
+        direction="down"
+      >
+        <CDropdownToggle className="c-header-nav-link drop-down" caret={false}>
+          <div className="pr-2">
+            <span className="text-muted">Hi: <strong>Admin</strong></span>
+          </div>
+          <div className="c-avatar">
+            <CImg
+              src={'avatars/6.jpg'}
+              className="c-avatar-img"
+              alt="admin@bootstrapmaster.com"
+            />
+          </div>
+        </CDropdownToggle>
+        <CDropdownMenu className="pt-2 mt-2" placement="bottom-end">
+          <CDropdownItem>
+            <CLink to=""><CIcon name="cil-user" className="mfe-2" />{t('user-topmenu.item-profile-info')}</CLink>
+          </CDropdownItem>
+          <CDropdownItem onClick={() => pageNavigate("user-settings")}>
+            <CIcon name="cil-settings" className="mfe-2" />
+            {t('user-topmenu.item-setting')}
+          </CDropdownItem>
+          <CDropdownItem divider />
+          <CDropdownItem onClick={userLogout}>
+            <Icon.BoxArrowRight className="mfe-2" />
+            {t('user-topmenu.item-logout')}
+          </CDropdownItem>
+        </CDropdownMenu>
+      </CDropdown>
+      {/* {show ? <div onClick={closeModalHandler} className="back-drop"></div> : null} */}
+      {/* <CButton color="ghost" onClick={() => setShow(true)}>holo</CButton>
+      <ModalProfile show={show} closeModalHandler={closeModalHandler} /> */}
+
+
+    </>
   )
 }
 
