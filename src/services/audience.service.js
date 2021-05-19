3987362f5ce5cc12fd7f77c6173497d296cc6387
Nguyen Trieu Vi <poiudau@gmail.com>
2021-05-19 16:02:08 +0700

    fix api audience

--- src/services/audience.service.js
+++ src/services/audience.service.js
@@ -1,15 +1,10 @@
 import { axiosInstance } from "./axios";
 class AudienceService {
   getAllAudience(page, limit) {
-    return axiosInstance.get(`/api/contact/list?page=${page}&limit=${limit}`);
-  }
-  getAudienceByChannelType(page, limit, type) {
-    return axiosInstance.get(
-      `/api/contact/list-by-channel-type?page=${page}&limit=${limit}&type=${type}`
-    );
+    return axiosInstance.get(`/api/audience/list?page=${page}&limit=${limit}`);
   }
   getDetailById(id) {
-    return axiosInstance.get(`/api/contact/profile/${id}`);
+    return axiosInstance.get(`/api/audience/detail/${id}`);
   }
 }
 
