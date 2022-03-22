6e1cca94339a05a9435d366b61a48feddfc638d3
Thanh Nguyen <>
2022-03-22 20:48:29 +0700

    feat: add route and component helloworld

--- src/_routes.js
+++ src/_routes.js
@@ -4,6 +4,7 @@ const Home = React.lazy(() => import("./views/pages/Home"));
 const TodayBetting = React.lazy(() => import("./views/events/TodayBetting"));
 const EventSummary = React.lazy(() => import("./views/events/EventSummary"));
 const UserBetHistory = React.lazy(() => import("./views/events/UserBetHistory"));
+const HelloWorld = React.lazy(() => import("./views/pages/HelloWorld"));
 
 const routes = [
   { path: "/", exact: true, name: 'Home', component: Home },
@@ -11,6 +12,7 @@ const routes = [
   { path: "/events/:id", exact: true, name: 'EventSummary', component: EventSummary },
   { path: "/event-user/:id", exact: true, name: 'UserBetHistory', component: UserBetHistory },
   // { path: "/dashboard", name: i18n.t('top-menu.br-dashboard'), component: Dashboard },
+  { path: "/hello-world", exact: true, name: 'HelloWorld', component: HelloWorld },
 ];
 
 export default routes;
